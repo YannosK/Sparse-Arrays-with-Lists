6bb4b6a408245f74795aeeb719ce362b76dfc13c
YannosK <giannos2405@gmail.com>
2023-10-15 00:31:15 +0300

    First attempt - works exept when del a single head

--- two_way_connected_list.c
+++ two_way_connected_list.c
@@ -1,3 +1,10 @@
+/*
+TWO WAY CONNECTED LIST
+
+a list of nodes that each one has an integer inside that dictates the position of the node
+the list is of ascending order with the head containing the smallest integer
+no duplicate nodes are allowed
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,8 +22,9 @@ struct node
 node_pointer head;
 
 void insert(node_pointer *head);
+int delete(node_pointer *head);
+void node(node_pointer head);
 void print(node_pointer head);
-void delete(node_pointer *head);
 
 
 
@@ -31,7 +39,7 @@ int main (void)
 	while (user_input != 'q')
 	{
 		printf("\nPrevious choice %c\n", user_input);
-		printf("q : quit, i : insert new node, p : prints nodes' data, P : prints nodes' data using recursive code, d: deletes node using its data\n");
+		printf("q : quit, i : insert new node, p : prints nodes' data, d: deletes node using its data, n: shows the connections of the node\n");
 		
 		if (user_input != 'q')
 		{
@@ -62,6 +70,12 @@ int main (void)
 	    		printf("\n\n\tDELETE\n\n");
 			    delete(&head);			    
 	    	}		
+
+			if (user_input == 'n')
+	    	{
+	    		printf("\n\n\tNODE CONNECTIONS\n\n");
+			    node(head);		    
+	    	}		
 		}
 	}
 	
@@ -71,7 +85,7 @@ int main (void)
 
 
 // inserts new node
-void insert (node_pointer *head)
+void insert (node_pointer *head) //warning: you pass a pointer to head as an argument because otherwise head won't change globally
 {
 	node_pointer new_node, aux; 
 	int new_node_data;
@@ -87,9 +101,9 @@ void insert (node_pointer *head)
 	new_node->data = new_node_data;
 	new_node->next = NULL;
 	new_node->back = NULL;
-
+	
 	// the case that the list is not empty
-	if ((*head) != NULL) //warning: head is a dereferneced pointer. new_node is not dereferenced
+	if ((*head) != NULL)
 	{
         // the case that the new node should go after head, so we traverse the list to find the proper spot
 		if (new_node->data > (*head)->data)
@@ -106,7 +120,7 @@ void insert (node_pointer *head)
             new_node->back = aux;
 	     	aux->next = new_node;
 		
-    		printf("\tData:%d\n\n\n", new_node->data);
+    		printf("\tData:%d\n\n", new_node->data);
 		}
 		
         // the case that the new node should go before head, so it becomes the new head
@@ -116,7 +130,7 @@ void insert (node_pointer *head)
 			(*head)->back = new_node;
     		*head = new_node;
 		
-    		printf("\tData:%d\n\n\n", new_node->data);
+    		printf("\tData:%d\n\n", new_node->data);
 		}
 	}
 
@@ -125,10 +139,208 @@ void insert (node_pointer *head)
 	{   
 		*head = new_node;
 		
-		printf("\tData:%d\n\n\n", new_node->data);
+		printf("\tData:%d\n\n", new_node->data);
 	}
 	
 }
+
+
+
+int delete(node_pointer *head) 
+{
+	node_pointer node_to_delete, aux, aux2;
+		
+	node_to_delete = (node_pointer)malloc(sizeof(struct node));
+		
+	if ((*head) == NULL)
+	{
+		printf("\tThe list is empty\n\n");
+	}
+	
+	if ((*head) != NULL)
+	{
+		int d;
+		printf("\tData of node to delete: ");
+	    scanf("%d", &d);
+	    getchar();
+	    
+	    aux = *head;
+
+		if (d < (*head)->data)
+		{
+			printf("\tNo such node was found. The node you added has data smaller than the head node\n\n");
+		}
+	    
+        // deleting something after the head
+	    if (d > (*head)->data)
+		{
+			// this catches the case that there is only the head and someone inputs something larger than it
+			if (aux->next==NULL)
+			{
+				printf("\tNo such node was found. All the nodes were parsed and your input node was not in the list\n\n");
+				return 0;
+			}
+
+		    while (aux->next->data < d  &&  aux->next != NULL)
+			{
+				aux = aux->next;
+
+				if (aux->next==NULL)
+				{
+					printf("\tNo such node was found. All the nodes were parsed and your input node was not in the list\n\n");
+					return 0;
+				}
+			}			
+			
+			if (aux->next->data == d)
+			{
+				node_to_delete = aux->next;
+				if (node_to_delete->next != NULL)
+				{
+					aux2 = node_to_delete->next; //not sure if proper way. But aux2 must be two nodes ahead of aux
+					aux->next = aux2;
+					aux2->back = aux;
+					free(node_to_delete);
+				}
+				else
+				{
+					aux->next = node_to_delete->next;
+					free(node_to_delete);
+				}
+			}
+			else
+			{
+				printf("\tNo such node was found. You added a value that is in between the list's nodes\n\n");		
+			}
+		}
+		
+		// deleting the head
+		if (d == (*head)->data  &&  (*head)->next != NULL)
+	    {
+	    	(*head) = aux->next;
+			(*head)->back = NULL;
+	    	free(aux);
+			printf("\n\n");   	
+		}
+		else if (d == (*head)->data  &&  (*head)->next == NULL)
+		{			
+			(*head) = NULL;
+			(*head)->next = NULL;
+			(*head)->back = NULL;
+			return 0;
+			free(aux);		
+		}
+	}
+}
+
+
+
+void node(node_pointer head)
+{
+	node_pointer aux;
+	aux = head;
+
+	if (head == NULL)
+	{
+		printf("\tThe list is empty\n\n");
+	}
+	
+	if (head != NULL)
+	{
+		int n;
+		printf("\tData of node: ");
+	    scanf("%d", &n);
+	    getchar();
+
+		if (n < head->data)
+		{
+			printf("\tNo such node was found. The node you added has data smaller than the head node\n\n");
+		}
+
+	    if (n > head->data)
+		{
+		    while (aux->data < n  &&  aux->next != NULL)
+			{
+				aux = aux->next;	
+			}
+
+			if (aux->data == n)
+			{
+				printf("\n\tData: %d", aux->data);
+
+				if (aux->next != NULL)
+				{
+					printf("\n\tNext: %d", aux->next->data);
+				}
+				else if (aux->next == NULL)
+				{
+					printf("\n\tNext: NULL");
+				}
+				else
+				{
+					printf("NEXT IS NOT CONNECTED");
+				}
+
+				if (aux->back != NULL)
+				{
+					printf("\n\tBack: %d\n\n", aux->back->data);
+				}
+				else if (aux->back == NULL)
+				{
+					printf("\n\tBack: NULL\n\n");
+				}
+				else
+				{
+					printf("BACK IS NOT CONNECTED\n\n");
+				}
+			}
+			else
+			{
+				if (aux->data > n)
+				{
+					printf("\tNo such node was found. Search stopped to a node closest to what you asked, but with larger data number\n\n");
+				}
+				if (aux->data < n)
+				{
+					printf("\tNo such node was found. Search stopped to the node with largest data number, but what you asked was even bigger\n\n");
+				}				
+			}			
+		}
+		
+		if (n == head->data)
+	    {	    	    	
+	    	printf("\n\tYou added the head");
+			printf("\n\tData: %d", aux->data);
+
+			if (aux->next != NULL)
+			{
+				printf("\n\tNext: %d", aux->next->data);
+			}
+			else if (aux->next == NULL)
+			{
+				printf("\n\tNext: NULL");
+			}
+			else
+			{
+				printf("NEXT IS NOT CONNECTED");
+			}
+
+			if (aux->back != NULL)
+			{
+				printf("\n\tBack: %d\n\n", aux->back->data);
+			}
+			else if (aux->back == NULL)
+			{
+				printf("\n\tBack: NULL\n\n");
+			}
+			else
+			{
+				printf("BACK IS NOT CONNECTED\n\n");
+			}	
+		}
+	}
+}
+
 
 
 void print (node_pointer head)
@@ -149,67 +361,4 @@ void print (node_pointer head)
 	    }
 	}
 	printf("\n");
-}
-
-
-// PROBLEMS
-void delete(node_pointer *head) 
-{
-	node_pointer node_to_delete, aux, aux2;
-		
-	node_to_delete = (node_pointer)malloc(sizeof(struct node));
-		
-	if ((*head) == NULL)
-	{
-		printf("\tThe list is empty\n\n");
-	}
-	
-	if ((*head) != NULL)
-	{
-		int d;
-		printf("\tData of node to delete: ");
-	    scanf("%d", &d);
-	    getchar();
-	    
-	    aux = *head;
-
-        // the case that the list does not only contain the head
-	    if (d != (*head)->data  &&  d > (*head)->data  &&  (*head) != NULL  &&  (*head)->next!=NULL)
-		{
-		    while (aux->next->data != d  &&  aux->next != NULL)
-			{
-				aux = aux->next;			
-			}
-			
-			node_to_delete = aux->next;
-
-			if (node_to_delete->next != NULL)
-			{
-				aux2 = node_to_delete->next; //not sure if proper way. But aux2 must be two nodes ahead of aux
-				aux->next = aux2;
-				aux2->back = aux;
-				free(node_to_delete);
-			}
-			else
-			{
-				aux->next = node_to_delete->next;
-				free(node_to_delete);
-			}
-		}
-		
-		if (d == (*head)->data  &&  (*head) != NULL  &&  (*head)->next != NULL)
-	    {	    	    	
-	    	(*head) = aux->next; //or (*head)->next
-			(*head)->back = NULL;
-	    	free(aux);	    	
-		}
-
-		if (d == (*head)->data  &&  (*head) != NULL  &&  (*head)->next == NULL)
-	    {	    	    	
-	    	(*head) = NULL;
-			(*head)->next = NULL;
-			(*head)->back = NULL; //might be redundant
-	    	free(aux);	    	
-		}
-	}
 }
